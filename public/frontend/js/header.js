e1b3b835b0efca169c34d0fe03c4e54a3fc81a44
hoangsang.nguyen2001a@gmail.com <hoangsang.nguyen2001a@gmail.com>
2022-02-18 17:52:55 +0700

    added header with input-search

--- public/frontend/js/header.js
+++ public/frontend/js/header.js
@@ -3,9 +3,8 @@ const inputSearchButton = $(".input-search-button");
 const inputSearchInputWrapper = $(".input-search__input-wrapper");
 const inputSearchBorder = $(".input-search__border");
 const headerInputSearchId = $("#header_input_search_id");
-const inputSearch = $(".input-search");
 
-inputSearchButton.onmouseover = function () {
+function openWrapperAndBorder() {
     if (
         inputSearchInputWrapper.classList.contains("wrapper-is-close") &&
         inputSearchBorder.classList.contains("border-is-close")
@@ -16,9 +15,9 @@ inputSearchButton.onmouseover = function () {
         inputSearchInputWrapper.classList.add("wrapper-is-open");
         inputSearchBorder.classList.add("border-is-open");
     }
-};
+}
 
-headerInputSearchId.onmouseout = function () {
+function closeWrapperAndBorder() {
     inputSearchInputWrapper.classList.remove("wrapper-is-open");
     inputSearchBorder.classList.remove("border-is-open");
 
@@ -26,6 +25,14 @@ headerInputSearchId.onmouseout = function () {
     inputSearchBorder.classList.add("border-is-close");
 
     inputSearchBorder.style.backgroundColor = "#b3b3b3";
+}
+
+inputSearchButton.onmouseover = function () {
+    openWrapperAndBorder();
+};
+
+headerInputSearchId.onblur = function () {
+    closeWrapperAndBorder();
 };
 
 headerInputSearchId.onfocus = function () {
