e86b84990ca7fdc2354d1fedb531a615429cb753
hoangsang.nguyen2001a@gmail.com <hoangsang.nguyen2001a@gmail.com>
2022-02-27 17:34:36 +0700

    refactor home.js

--- public/frontend/js/home.js
+++ public/frontend/js/home.js
@@ -2,6 +2,5 @@ const dispatchElementChild = $$(".dispatch-element");
 
 window.onload = () => {
     dispatchElementChild[0].style.transform = "translateX(0)";
-
     dispatchElementChild[1].style.transform = "translateX(0)";
 };
