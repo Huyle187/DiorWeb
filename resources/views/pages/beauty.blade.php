7ed88324130b79dcbabe7d6ee3750c861b3a952e
hoangsang.nguyen2001a@gmail.com <hoangsang.nguyen2001a@gmail.com>
2022-02-28 18:33:16 +0700

    added beauty page

--- resources/views/pages/beauty.blade.php
+++ resources/views/pages/beauty.blade.php
@@ -3,7 +3,10 @@
 @section('title', 'DIOR Beauty: Cosmetics, Fragrances, Skincare & Gifts | DIOR')
 
 @section("styles")
-    <link rel="stylesheet" type="text/css" href="{{ ('public/frontend/css/beauty.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ ('public/frontend/css/fashion.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ ('public/frontend/css/collections.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ ('public/frontend/css/service-messaging.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ ('public/frontend/css/footer-explore-more.css') }}">
 @stop
 
 @section("responsive")
@@ -11,11 +14,19 @@
 @stop
 
 @section('content')
-    <section class="beauty">
-        beauty
+    <section class="fashion">
+        @include("components.collections")
+        @include("components.collections")
+        @include("components.collections")
+        @include("components.collections")
+        @include("components.collections")
     </section>
+
+    @include("components.service-messaging")
+    @include("components.footer-explore-more")
 @stop
 
 @section('scripts')
-    <script src="{{ ('public/frontend/js/beauty.js') }}" crossorigin="anonymous"></script>
+    <script src="{{ ('public/frontend/js/service-messaging.js') }}" crossorigin="anonymous"></script>
+    <script src="{{ ('public/frontend/js/fashion.js') }}" crossorigin="anonymous"></script>
 @stop
