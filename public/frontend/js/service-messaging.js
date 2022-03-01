9d975df9b902fa1d6150c443ad47d62cc6645d4d
hoangsang.nguyen2001a@gmail.com <hoangsang.nguyen2001a@gmail.com>
2022-03-01 18:24:21 +0700

    refactor

--- public/frontend/js/service-messaging.js
+++ public/frontend/js/service-messaging.js
@@ -1,19 +1,21 @@
-function transformFromRightToLeft() {
-    const serviceMessagingAnimated = $(".service-messaging-animated");
+const serviceMessagingAnimated = $(".service-messaging-animated");
 
+setInterval(() => {
     serviceMessagingAnimated.style.transform = "translateX(0)";
-    setTimeout(function () {
+
+    setTimeout(() => {
         serviceMessagingAnimated.style.transform = "translateX(30%)";
     }, 1000);
-    setTimeout(function () {
+
+    setTimeout(() => {
         serviceMessagingAnimated.style.transform = "translateX(60%)";
     }, 2000);
-    setTimeout(function () {
+
+    setTimeout(() => {
         serviceMessagingAnimated.style.transform = "translateX(90%)";
     }, 3000);
-    setTimeout(function () {
+
+    setTimeout(() => {
         serviceMessagingAnimated.style.transform = "translateX(120%)";
     }, 4000);
-}
-transformFromRightToLeft();
-setInterval(transformFromRightToLeft, 5000);
+}, 5000);
